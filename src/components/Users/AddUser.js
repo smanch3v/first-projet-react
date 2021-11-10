df1c478c0843efa63d99e1b47789b2c48aaee81c
smanch3v <svetoslavmanchev@gmail.com>
2021-11-10 15:30:36 +0100

    lifting state up addded / new users pass to another component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [eneteredAge, setEneteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (
+      enteredUsername.trim().length === 0 ||
+      eneteredAge.trim().length === 0
+    ) {
+      return;
+    }
+    if (+eneteredAge < 1) {
+      return;
+    }
+
+    props.onAddUser(enteredUsername, eneteredAge);
+    //console.log(enteredUsername, eneteredAge);
+    setEnteredUsername(""); //this is how we refresh the value after submit
+    setEneteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEneteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={eneteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+import classes from "./UsersList.module.css";
+import Card from "../UI/Card";
+
+const UsersList = (props) => {
+//   useState;
+
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
